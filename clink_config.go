68c9e3738eee699f5f486ea82601ab5c63be9495
Cameron Bergoon <cbergoon@gmail.com>
2016-09-21 10:05:43 -0400

    Fixing bug in syntax

--- clink_config.go
+++ clink_config.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 )
 
+// CmdMode describes the current mode Clink is running in. Describing the actions
+// which are available to take.
 type CmdMode int
 
+// CmdMode definition constants.
 const (
 	MNGE CmdMode = iota
 	EXEC
@@ -14,14 +17,27 @@ const (
 	UNDF
 )
 
+// ExecMode describes the type of operation to run on the execution. Describes the
+// type of scan to run.
 type ExecMode int
 
+// ExecMode definition constants.
 const (
 	ECHOMON ExecMode = iota
 	PSCNMON
 	UNDFMON
 )
 
+// Default Clink configuration values
+const (
+	DEFAULT_MODE        CmdMode  = 1
+	DEFAULT_EXEC_MODE   ExecMode = 0
+	DEFAULT_CONFIG_FILE string   = "clink.conf"
+	DEFAULT_HOST_URI    string   = ""
+)
+
+// ClinkConfig is the configuration for the Clink command. Detailing how the command will
+// execute.
 type ClinkConfig struct {
 	cmdMode    CmdMode
 	execMode   ExecMode
@@ -29,21 +45,31 @@ type ClinkConfig struct {
 	hostUri    string
 }
 
-// Default Clink configuration values
-const (
-	DEFAULT_MODE      CmdMode  = 1
-	DEFAULT_EXEC_MODE ExecMode = 0
-)
-
+// NewClinkConfig creates a new clink configuration. Note: the values take the zero value
+// rather than the default values defined above.
 func NewClinkConfig() *ClinkConfig {
 	return &ClinkConfig{}
 }
 
+// commandUsage provides a custom usage prompt to overcome readablilty challenges
+// presented by default flags implementation.
 func commandUsage() {
 	fmt.Printf("Usage: clink -[m|e|r[options]] -[echo|scan[options]] -[host|file]>\n")
-	flag.PrintDefaults()
+	fmt.Printf("	Command Mode\n")
+	fmt.Printf("		-m 	Management mode\n")
+	fmt.Printf("		-e 	Execute mode\n")
+	fmt.Printf("		-r 	Report mode\n")
+	fmt.Printf("	Execute Mode\n")
+	fmt.Printf("		-echo 	Execute icmp echo monitoring\n")
+	fmt.Printf("		-pscan 	Execute port scan monitoring\n")
+	fmt.Printf("	Input Description\n")
+	fmt.Printf("		-file 	Configuration file to describe execution of monitoring task\n")
+	fmt.Printf("		-host 	Configure clink to launch scan on defined host\n")
 }
 
+// HandleFlags initializes the potential settings and parses their value into the config
+// object. Care should be taken to update `commandUsage` function to reflect any changes
+// to this function.
 func (c *ClinkConfig) HandleFlags() {
 	//Set Default Usage
 	flag.Usage = commandUsage
@@ -86,6 +112,7 @@ func (c *ClinkConfig) HandleFlags() {
 
 }
 
+// Process to follow...
 func (c *ClinkConfig) Process() {
 	return
 }
